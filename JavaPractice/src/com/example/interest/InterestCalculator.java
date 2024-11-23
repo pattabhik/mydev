52ced61266e31b1cbeb6adb5035e2790a1b83573
pattabhik <pattabhiramireddy.k@gmail.com>
2024-11-23 16:59:51 +0530

    new changes

--- JavaPractice/src/com/example/interest/InterestCalculator.java
+++ JavaPractice/src/com/example/interest/InterestCalculator.java
@@ -8,19 +8,19 @@ public class InterestCalculator {
 
 	public static void main(String[] args) {
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		String fromDtStr = "01/01/2024";
-		String toDtStr = "20/02/2024";
+		String fromDtStr = "26/04/2024";
+		String toDtStr = "25/04/2025";
 
 		// year,month,day
 		LocalDate fromDate = LocalDate.parse(fromDtStr, dtf);
 		LocalDate toDate = LocalDate.parse(toDtStr, dtf);
-		long noOfDays = ChronoUnit.DAYS.between(fromDate, toDate);
-		int principal = 400000;
-		double rate = (double) 2.0 / 100;
+		long noOfDays = ChronoUnit.DAYS.between(fromDate, toDate) + 1;
+		int principal = 200000;
+		double rate = (double) 2 / 100;
 		double months = (double) noOfDays / 30;
 		double intrst = principal * rate * months;
 		System.out.println("To Pay:" + (principal + intrst) + ", Principal:" + principal + ", " + "Days: " + noOfDays
-				+ ", Interest: " + intrst + ", Months: " + months);
+				+ "(" + months + " months), Interest: " + intrst);
 	}
 
 }
