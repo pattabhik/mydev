a90ab4fd8fd5cf322d89565dac157b7041c4f889
pattabhik <58624314+pattabhik@users.noreply.github.com>
2024-12-31 13:47:12 +0000

    Create ClassProps.js

---
+++ react-app/src/components/ClassProps.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class ClassProps extends Component{
+    render(){
+        return <h2>Content from {this.props.name}</h2>
+    }
+}
+
+export default ClassProps;
